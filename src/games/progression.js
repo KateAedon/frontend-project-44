c8d12776172fbec851948a0fa8c01bf9f95b6631
red-edit <kate.aedon@gmail.com>
2024-06-24 10:14:49 +0200

    Change the progression function to the deterministic

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,12 +2,11 @@ import game from '../index.js';
 import { getRandomNumber, getRandomIndex } from '../utils.js';
 
 const description = 'What number is missing in the progression?';
+const initialValue = getRandomNumber(1, 30);
+const difference = getRandomNumber(1, 30);
 
-const makeProgression = () => {
-  const initialValue = getRandomNumber(1, 30);
-  const difference = getRandomNumber(1, 30);
-  const length = 10; // длина задана в тех.задании
-
+const makeProgression = (initialValue, difference) => {
+  const length = 10; // длина задана в тех.задании  
   const progression = [];
 
   for (let i = 0; i < length; i += 1) {
@@ -28,7 +27,7 @@ const hideNumber = (progression) => {
 /* eslint-enable no-param-reassign */
 
 const getTask = () => {
-  const progression = makeProgression();
+  const progression = makeProgression(initialValue, difference);
   const { progression: progressionWithHiddenNumber, hiddenNumber } = hideNumber(progression);
   const question = progressionWithHiddenNumber.join(' ');
   const correctAnswer = String(hiddenNumber);
